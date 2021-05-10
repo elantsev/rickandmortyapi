1d2aa5e3181ec2e055ce141bc04227da124a08a3
elantsev1 <elantsev.pa@gmail.com>
2021-05-10 23:15:18 +0300

    add Redirect to="/characters"

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Redirect,
 } from "react-router-dom";
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
               <Characters />
               <PaginationLink />
             </Route>
+            <Redirect to="/characters" />
           </Switch>
         </header>
       </Box>
